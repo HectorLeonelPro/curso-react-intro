bb68385d637534308ea36ca867e290ef483e78de
HectorLeonelPro <149825836+HectorLeonelPro@users.noreply.github.com>
2024-05-20 20:59:17 -0600

    wer
    sdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './platzi.webp';
-import './App.css';
+import {TodoCounter} from './TodoCounter'
+import {TodoSearch} from './TodoSearch'
+import {TodoList} from './TodoList'
+import {TodoItem} from './TodoItem';
+import {CreateTodoButton} from './CreateTodoButton'
+import React from 'react';
+
+const defaultTodos = [
+	{text: 'Cortar cebolla', completed: false},
+	{text: 'Cortar cebolla 2', completed: true},
+	{text: 'Cortar cebolla 3', completed: false},
+	{text: 'Cortar cebolla 4', completed: false},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = React.useState(defaultTodos);
+	const [searchValue, setSearchValue] = React.useState('');
+	console.log('Los usuarios buscan todos de ' + searchValue)
+
+	const completedTodos = todos.filter(todo => !!todo.completed).length;
+	const totalTodos = todos.length;
+	const searchedTodos = todos.filter(todo => {
+		return todo.text.includes(searchValue)
+	});
+
+	return (
+		<React.Fragment>
+
+			<TodoCounter completed={completedTodos} total={totalTodos} />
+			<TodoSearch 
+				searchValue={searchValue}
+				setSearchValue={setSearchValue}
+			/>
+			
+			<TodoList>
+				{searchedTodos.map(todo => (
+					<TodoItem 
+						key={todo.text} 
+						text={todo.text}
+						completed={todo.completed}
+					/>
+				))}
+			</TodoList>
+
+			<CreateTodoButton />
+
+		</React.Fragment>
+	);
 }
 
 export default App;
